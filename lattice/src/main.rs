da67ca45f1b828c3c7509543fd3ff69f43d48a98
johnye <yezhuoyang98@g.ucla.edu>
2024-11-11 09:24:25 -0800

    Second

--- lattice/src/main.rs
+++ lattice/src/main.rs
@@ -9,7 +9,7 @@ fn main(){
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
 
-    println("The secret number is : {secret_number}");
+    println!("The secret number is : {secret_number}");
 
     println!("Please input your guess");
 
